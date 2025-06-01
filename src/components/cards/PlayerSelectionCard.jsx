78e6163bcc27eb3985f2de64f227ce048276f779
Carlos de Leon <admin@koxland.net>
2025-06-01 16:23:43 -0600

    Update PlayerSelectionCard.jsx

--- src/components/cards/PlayerSelectionCard.jsx
+++ src/components/cards/PlayerSelectionCard.jsx
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from "react";
 import Slider from "../buttons/Slider";
 
 function PlayerSelectionCard() {
-  const [userSelection, setUserSelection] = useState(true);
+  const [userSelection, setUserSelection] = useState('O');
 
   const handleSelection = (symbol) => {
     setUserSelection(symbol);
     console.log("Selected symbol:", symbol);
   };
-
-  const handleSrcChange = "solid-dark";
 
   return (
     <div className="w-full sm:w-[460px] h-52 bg-semi-dark-navy rounded-2xl p-6 gap-6 flex flex-col shadow-[0_6px_0_var(--slate-900)]">
@@ -18,7 +16,7 @@ function PlayerSelectionCard() {
         <Slider userSelection={userSelection} onSelect={handleSelection} />
         <div
           className={`
-    border border-blue-600 w-[132px] sm:w-[198px] h-[54px] bg-silver rounded-xl z-0 absolute top-1/2 -translate-y-1/2 transition-all duration-300 ease-in-out
+    border border-blue-600 w-[132px] sm:w-[198px] h-[54px] bg-silver rounded-xl z-0 absolute top-1/2 -translate-y-1/2 transition-all bg- duration-300 ease-in-out
     left-3 sm:left-2
     ${userSelection === "X" ? "translate-x-0" : "translate-x-[125px] sm:translate-x-[195px]"}
   `}
