88c8ff93675ce2ab2b92c62d08eb711868bafe9b
Carlos de Leon <admin@koxland.net>
2025-06-01 17:24:03 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./pages/HomeScreen";
 import BoardScreen from "./pages/BoardScreen";
-import Options from "./components/feedback/Options";
-import RestartTied from "./components/feedback/RestartTied";
 
 function App() {
   return (
@@ -15,10 +13,6 @@ function App() {
             <Route path="/board" element={<BoardScreen />} />
           </Routes>
         </Router>
-        {/* <HomeScreen /> */}
-        {/* <Options text1="quit" text2="next round" color="yellow" title="oh no, you lost..." subtitle="takes the round" /> */}
-        {/* <RestartTied text1="no, cancel" text2="yes, restart" color="yellow" title="restart game?" /> */}
-        {/* <BoardScreen /> */}
       </div>
     </>
   );
