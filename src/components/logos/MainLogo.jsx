27711084f2c13d8ee56201c0f02a013083fbbfcf
Carlos de Leon <admin@koxland.net>
2025-06-19 15:26:29 -0600

    Update MainLogo.jsx

--- src/components/logos/MainLogo.jsx
+++ src/components/logos/MainLogo.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function MainLogo() {
-  return (
-    <img
-    className='' 
-    src="/src/assets/logo.svg" alt="logo icon" />
-  )
+  return <img className="" src="/assets/logo.svg" alt="logo icon" />;
 }
 
-export default MainLogo
+export default MainLogo;
